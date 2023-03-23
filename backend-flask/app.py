8f85470c959a449ec11178968d4797201a44b766
lahham392 <yousefallahham@gmail.com>
2023-03-23 07:58:10 +0000

    Commit activities

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -176,7 +176,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'andrewbrown'
+  user_handle  = 'lahham'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
